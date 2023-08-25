aac879fc46657b2ce18357a425ee44ec07f6a2b4
ALIPHATIC <adegokefuad1@gmail.com>
2023-08-25 09:04:33 +0100

    refactoring

--- src/count-plugin.ts
+++ src/count-plugin.ts
@@ -1,40 +1,49 @@
 import { PluginConfig, PluginInfo } from '@paljs/types';
 import { gql } from 'graphql-tag';
-import { IEntityMetadata, IEntityController, EntityController } from '@paljs/types';
+import { IEntityController, EntityController } from '@paljs/types';
 import { createPlugin } from '@paljs/editor';
 
+// Defining a function to create the count method for an entity controller
+function createCountMethod(controller: EntityController<any>) {
+  controller.service.addMethod(
+    'count',
+    gql`
+      type CountResult {
+        count: Int!
+      }
+
+      extend type Query {
+        count${controller.entityName}s: CountResult!
+      }
+    `,
+    async () => {
+      const count = await controller.service.count();
+      return { count };
+    }
+  );
+}
+
+// Defining the countPlugin configuration
 const countPlugin: PluginConfig = {
   entitiesMetadataHooks: {
     async beforeApplyMetadata(
-      entitiesMetadata: IEntityMetadata[],
+      _,
       entityControllers: IEntityController[]
     ) {
-      // Find the entity controller for which you want to add the count functionality
+      // Find the entity controller for 'User'
       const userController = entityControllers.find(
         (controller) => controller.entityName === 'User'
-      ) as EntityController<'User'>;
-
-      // Modify the REST API Controller to expose the count method
-      userController.service.addMethod(
-        'count',
-        gql`
-          type CountResult {
-            count: Int!
-          }
-
-          extend type Query {
-            countUsers: CountResult!
-          }
-        `,
-        async () => {
-          const count = await userController.service.count();
-          return { count };
-        }
       );
+
+      // Add count functionality if the controller is found
+      if (userController) {
+        createCountMethod(userController);
+      }
     },
   },
 };
 
+// Exporting the plugin information and configuration
 export default createPlugin(
   (): PluginInfo => ({
     name: 'count-plugin',
@@ -44,10 +53,3 @@ export default createPlugin(
   }),
   countPlugin
 );
-
-function createPlugin(arg0: () => PluginInfo, countPlugin: PluginConfig) {
-  // The implementation of createPlugin is missing, it should be provided elsewhere.
-  // This function should return the plugin information and configuration.
-  // For this example, we can simply return the provided countPlugin.
-  return countPlugin;
-}
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,36 @@
 import { PluginInstallation } from "@amplication/code-gen-types";
-import { name as PackageName } from "../package.json"
-import { Settings } from './types'
+import { name as PackageName } from "../package.json";
+import { Settings } from './types';
 import defaultSettings from "../.amplicationrc.json";
 
+/**
+ * Retrieving and merging plugin settings from default settings and user-defined settings.
+ *
+ * @param {PluginInstallation[]} pluginInstallations - An array of plugin installations.
+ * @returns {Settings} The merged plugin settings.
+ * @throws {Error} If the plugin installation is not found.
+ */
 export const getPluginSettings = (
-    pluginInstallations: PluginInstallation[]
-  ): Settings => {  
-    const plugin = pluginInstallations.find((plugin) => plugin.npm === PackageName);
+  pluginInstallations: PluginInstallation[]
+): Settings => {
+  // Finding the plugin installation by package name
+  const plugin = pluginInstallations.find(
+    (installation) => installation.npm === PackageName
+  );
 
-    const userSettings = plugin?.settings ?? {};
-
-    const settings : Settings = {
-        ...defaultSettings,
-        ...userSettings  
-    }
- 
-    return settings;
+  // If the plugin installation is not found, throw an error
+  if (!plugin) {
+    throw new Error(`Plugin '${PackageName}' is not installed.`);
   }
+
+  // Extract user-defined settings or use an empty object if not defined
+  const userSettings = plugin.settings || {};
+
+  // Merge default settings with user-defined settings
+  const settings: Settings = {
+    ...defaultSettings,
+    ...userSettings,
+  };
+
+  return settings;
+};
